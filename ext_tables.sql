c89694cd8769a3d62206edc05ef001ea79ac8aa3
Xavier Perseguers <xavier@causal.ch>
2024-03-21 17:03:58 +0100

    [TASK] Create structure in database

---
+++ ext_tables.sql
@@ -0,0 +1,9 @@
+CREATE TABLE tx_staffdirectory_domain_model_organization
+(
+    tx_staffdirectoryorganization_address       varchar(100) DEFAULT '' NOT NULL,
+    tx_staffdirectoryorganization_postal_code   varchar(10)  DEFAULT '' NOT NULL,
+    tx_staffdirectoryorganization_city          varchar(50)  DEFAULT '' NOT NULL,
+    tx_staffdirectoryorganization_email         varchar(80)  DEFAULT '' NOT NULL,
+    tx_staffdirectoryorganization_telephone     varchar(20)  DEFAULT '' NOT NULL,
+    tx_staffdirectoryorganization_opening_hours text,
+);
