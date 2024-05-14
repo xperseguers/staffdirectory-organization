78306ca738615426c0fc48e70348e5e095edbdfb
Hasan Shaker <shaker@hemmer.ch>
2024-05-14 10:10:55 +0200

    [FEATURE] Add images for organization

--- Configuration/TCA/Overrides/tx_staffdirectory_domain_model_organization.php
+++ Configuration/TCA/Overrides/tx_staffdirectory_domain_model_organization.php
@@ -1,6 +1,5 @@
 <?php
 defined('TYPO3') || die();
-
 $tempColumns = [
     'tx_staffdirectoryorganization_address' => [
         'exclude' => false,
@@ -62,6 +61,19 @@ $tempColumns = [
             'rows' => 4,
         ],
     ],
+    'tx_staffdirectoryorganization_images' => [
+        'exclude' => false,
+        'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.image',
+        'l10n_mode' => 'exclude',
+        'config' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::getFileFieldTCAConfig(
+            'tx_staffdirectoryorganization_images',
+            [
+                'maxitems' => 5,
+                'minitems' => 0
+            ],
+            $GLOBALS['TYPO3_CONF_VARS']['GFX']['imagefile_ext']
+        )
+    ],
 ];
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns(
@@ -78,7 +90,9 @@ $tempColumns = [
             tx_staffdirectoryorganization_locality,
             tx_staffdirectoryorganization_email,
             tx_staffdirectoryorganization_telephone,
-            tx_staffdirectoryorganization_opening_hours
+            tx_staffdirectoryorganization_opening_hours,
+        --div--;LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.images,
+            tx_staffdirectoryorganization_images,
     ',
     '',
     'after:description'
