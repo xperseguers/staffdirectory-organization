d79b2ac6df58e782609321555d6ba55168a8cadf
Xavier Perseguers <xavier@causal.ch>
2024-05-27 12:43:28 +0200

    [TASK] Add support for an organization's website
    Related: #3

--- Configuration/TCA/Overrides/tx_staffdirectory_domain_model_organization.php
+++ Configuration/TCA/Overrides/tx_staffdirectory_domain_model_organization.php
@@ -31,6 +31,18 @@ $tempColumns = [
             'max' => 10,
         ],
     ],
+    'tx_staffdirectoryorganization_website' => [
+        'exclude' => false,
+        'label' => 'LLL:EXT:staffdirectory_organization/Resources/Private/Language/locallang_db.xlf:tx_staffdirectory_domain_model_organization.website',
+        'config' => [
+            'type' => 'input',
+            'size' => 30,
+            'eval' => 'trim',
+            'max' => 100,
+            'softref' => 'typolink',
+            'placeholder' => 'https://www.domain.tld',
+        ],
+    ],
     'tx_staffdirectoryorganization_email' => [
         'exclude' => false,
         'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.email',
--- ext_tables.sql
+++ ext_tables.sql
@@ -5,6 +5,7 @@ CREATE TABLE tx_staffdirectory_domain_model_organization
     tx_staffdirectoryorganization_locality      varchar(50)  DEFAULT '' NOT NULL,
     tx_staffdirectoryorganization_email         varchar(80)  DEFAULT '' NOT NULL,
     tx_staffdirectoryorganization_telephone     varchar(20)  DEFAULT '' NOT NULL,
+    tx_staffdirectoryorganization_website       varchar(100) DEFAULT '' NOT NULL,
     tx_staffdirectoryorganization_opening_hours text,
-    tx_staffdirectoryorganization_images         int(50)      DEFAULT '0' NOT NULL,
+    tx_staffdirectoryorganization_images        int(50)      DEFAULT '0' NOT NULL,
 );
